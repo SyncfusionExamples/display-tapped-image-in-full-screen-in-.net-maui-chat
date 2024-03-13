141f2f3c51b6c7851cc53d8805103dcdc8efc827
Lakshmi Natarajan <lakshmi.natarajan@syncfusion.com>
2024-03-13 18:42:32 +0530

    Added files for sample

---
+++ MauiChat/MauiChat/Behavior/Behavior.cs
@@ -0,0 +1,67 @@
+﻿using Syncfusion.Maui.Chat;
+using Syncfusion.Maui.Core;
+using Syncfusion.Maui.Popup;
+using MauiChat;
+
+namespace MauiChat
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        #region Fields
+
+        private SfChat? chat;
+        private SfPopup? popup;
+
+        #endregion
+
+        #region Override Methods
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            this.chat = bindable!.FindByName<SfChat>("sfChat");
+            if (this.chat != null)
+            {
+                this.chat.ImageTapped += Chat_ImageTapped;
+            }
+
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            if (this.chat != null)
+            {
+                this.chat.ImageTapped -= Chat_ImageTapped;
+                this.chat = null;
+            }
+
+            popup = null;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+
+        #region Private Methods
+        private void Chat_ImageTapped(object? sender, ImageTappedEventArgs e)
+        {
+            popup = new SfPopup();
+
+            popup.ShowCloseButton = true;
+            popup.HeaderTitle = "";
+            popup.PopupStyle.PopupBackground = Colors.Black;
+
+            DataTemplate bodyTemplateView = new DataTemplate(() =>
+            {
+                var imageView = new Image();
+                imageView.Source = e.Message?.Source;
+                imageView.Margin = new Thickness(0, 0, 0, 20);
+                return imageView;
+            });
+
+            popup.ContentTemplate = bodyTemplateView;
+            popup.Show(true);
+        }
+
+        #endregion        
+    }
+}
+
+
---
+++ MauiChat/MauiChat/ViewModel/ViewModel.cs
@@ -0,0 +1,107 @@
+﻿using Syncfusion.Maui.Chat;
+using Syncfusion.Maui.Popup;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace MauiChat
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private ObservableCollection<object> messages;
+        /// <summary>
+        /// current user of chat.
+        /// </summary>
+        private Author currentUser;
+
+        #endregion
+
+        #region Constructor
+        public ViewModel()
+        {
+            this.messages = new ObservableCollection<object>();
+            this.currentUser = new Author() { Name = "Nancy", Avatar = "peoplecircle16.png" };
+            this.GenerateMessages();
+        }
+        #endregion
+
+        #region Properties
+
+
+        /// <summary>
+        /// Gets or sets the group message conversation.
+        /// </summary>
+        public ObservableCollection<object> Messages
+        {
+            get
+            {
+                return this.messages;
+            }
+
+            set
+            {
+                this.messages = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current author.
+        /// </summary>
+        public Author CurrentUser
+        {
+            get
+            {
+                return this.currentUser;
+            }
+            set
+            {
+                this.currentUser = value;
+                RaisePropertyChanged("CurrentUser");
+            }
+        }
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        /// <summary>
+        /// Property changed handler.
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Occurs when property is changed.
+        /// </summary>
+        /// <param name="propName">changed property name</param>
+        public void RaisePropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+        #endregion
+
+        #region Messages Generation
+        private void GenerateMessages()
+        {
+            this.Messages.Add(new ImageMessage()
+            {
+                Aspect = Aspect.AspectFill,
+                Source = "image2.jpg",
+                Author = currentUser,               
+            });
+
+            this.Messages.Add(new ImageMessage()
+            {
+                Aspect = Aspect.AspectFill,
+                Source = "image1.jpg",
+                Author = new Author() { Name = "Andrea", Avatar = "peoplecircle2.png" },
+            });
+        }
+
+        #endregion    
+    }
+}
+
